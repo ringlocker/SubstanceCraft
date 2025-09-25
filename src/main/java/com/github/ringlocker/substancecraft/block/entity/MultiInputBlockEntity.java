7c694dcf8b7ac99477bbc43aff27db27f2480456
ringlocker <ringlocker9@gmail.com>
2025-09-25 09:40:08 -0600

    useless commit

--- src/main/java/com/github/ringlocker/substancecraft/block/entity/MultiInputBlockEntity.java
+++ src/main/java/com/github/ringlocker/substancecraft/block/entity/MultiInputBlockEntity.java
@@ -3,9 +3,6 @@ package com.github.ringlocker.substancecraft.block.entity;
 import com.github.ringlocker.substancecraft.recipe.generic.MultipleInputRecipe;
 import com.github.ringlocker.substancecraft.recipe.generic.MultipleItemInput;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeHolder;
@@ -14,7 +11,6 @@ import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
